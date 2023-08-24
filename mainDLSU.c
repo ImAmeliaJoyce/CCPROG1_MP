0bb6ee3ca8a1b60456a25064d93c2ac379d3c0ad
ImAmeliaJoyce <amelia_abenoja@dlsu.edu.ph>
2023-08-24 13:12:39 +0800

    v1 of mainDLSU

--- mainDLSU.c
+++ mainDLSU.c
@@ -1,10 +1,29 @@
 //<Preprocessor directives>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 #include <time.h>
 
+#include "funcGamePlay.c"
+
+
 int main(void) {
+    /* Variables */
+    int nNumPlayers = 0;
+    int i = 0;
+
+    char cResponse;
+    bool bPlayAgain = false;
+
+    do {
+        displayHeader();
+
+        bPlayAgain = wantPlayAgain();
+
+    }while(bPlayAgain);
 
     printf("\n\nHope you enjoyed Doggos, Ladders, Slides and U-turns!\nThank you for playing! :)");
+
+    return 0;
 }
